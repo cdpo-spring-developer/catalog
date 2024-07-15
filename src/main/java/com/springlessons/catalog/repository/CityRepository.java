02dbf307b2f3e68b3ec547b718cf8dfd294ec882
Dmitry <smirnoff.spb78@yandex.ru>
2024-07-15 20:18:45 +0300

    task2

---
+++ src/main/java/com/springlessons/catalog/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.springlessons.catalog.repository;
+
+import com.springlessons.catalog.entity.City;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CityRepository extends CrudRepository<City, Integer> {
+
+}
